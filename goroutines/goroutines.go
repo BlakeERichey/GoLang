b6999c259425c663f755acb422e6bff80e00709a
Blake Richey <spsprogrammer@SPSs-Mac-mini.local>
2019-03-25 08:43:46 -0500

    Added multiple channels and select statement examples

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -31,10 +31,44 @@ func main() {
 
 	//---------------Intro to Channels---------------
 	//Prints ping every 1 second
-	var c = make(chan string) //initialize channel
+	//var c = make(chan string) //initialize single channel
+	fmt.Println("Hit enter to end program...")
 
-	go pinger(c)
-	go printer(c)
+	// go pinger(c)
+	// go ponger(c) //second function on same channel
+	// go printer(c)
+
+	//---------------Multiple Channels---------------
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "1"
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "2"
+		}
+	}()
+
+	//select which channel currently has information ready to be received
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			case <-time.After(time.Second): //send time to new channel after 1 second
+				fmt.Println("timeout...")
+			default:
+				fmt.Println("Nothing Ready.")
+			}
+		}
+	}()
 
 	var input string
 	fmt.Scanln(&input) //hit enter to end program
@@ -54,5 +88,11 @@ func printer(c chan string) {
 		msg := <-c //receives info from channel c
 		fmt.Println(msg)
 		time.Sleep(time.Second * 1)
+	}
+}
+
+func ponger(c chan string) {
+	for {
+		c <- "pong"
 	}
 }
