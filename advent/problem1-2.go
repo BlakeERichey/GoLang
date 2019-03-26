fdf2fe311667245d566f3d122bbd863283f08ea1
Blake Richey <spsprogrammer@SPSs-Mac-mini.local>
2019-03-26 16:52:34 -0500

    Working on reading values in as ints instead of bytes

--- advent/problem1-2.go
+++ advent/problem1-2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 )
 
 func check(e error) {
@@ -28,22 +29,29 @@ func main() {
 	data, err := ioutil.ReadFile("input.txt")
 	check(err)
 
-	states := make([]int, 1) //values reached
-	current := 0             //current state value
-	index := 0               //current index in data
-	run := true
-	for run {
-		if index >= len(data) {
-			index = 0
-		}
-		current += int(data[index])
-		states = append(states, current)
-		run = !contains(states, current)
-		index++
-		if index > 5 {
-			break
-		}
+	nums := make([]int, 5)
+	for i := 0; i < 5; i++ {
+		fmt.Println(data[i])
+		nums[i], _ = strconv.Atoi(string(data[i]))
 	}
-	fmt.Println("First repeat", current)
-	fmt.Println(states)
+	fmt.Println(nums[0:5])
+
+	// states := make([]int, 1) //values reached
+	// current := 0             //current state value
+	// index := 0               //current index in data
+	// run := true
+	// for run {
+	// 	if index >= len(data) {
+	// 		index = 0
+	// 	}
+	// 	current += int(data[index])
+	// 	states = append(states, current)
+	// 	run = !contains(states, current)
+	// 	index++
+	// 	if index > 5 {
+	// 		break
+	// 	}
+	// }
+	// fmt.Println("First repeat", current)
+	// fmt.Println(states)
 }
