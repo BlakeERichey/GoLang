a1cd605b45180677335d73e0b5e29590aeb92369
Blake <blake.e.richey@gmail.com>
2019-04-02 20:50:07 -0500

    Added comments for clarity

--- Tutorials/import/folder/secondary.go
+++ Tutorials/import/folder/secondary.go
@@ -1,9 +1,9 @@
-package secondary //all files in folder "folder"
+package secondary //all files in folder "folder" must be of package secondary
 
 import "fmt"
 
 //Add Comment Required
-func Add(x, y int) int {
+func Add(x, y int) int { //must be capitalized
 	fmt.Println("In add")
 	return x + y
 }
