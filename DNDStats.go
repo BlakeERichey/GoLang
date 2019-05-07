bc8b480db9a2a467e55cab4a92e21e378cb104b7
Blake Richey <spsprogrammer@SPSs-Mac-mini.local>
2019-05-07 14:57:15 -0500

    Added named return statements

--- DNDStats.go
+++ DNDStats.go
@@ -8,60 +8,61 @@ import (
 func main() {
 	//get stats
 	stats := getStats()
-	fmt.Println(stats)
-	
+	//fmt.Println(stats)
+
 	//get best stats
 	count := 0
-	for avg(stats) < 17{
+	for avg(stats) < 17.1 {
 		count++
 		stats = getStats()
 	}
-	fmt.Println(stats, avg(stats), count)
+	fmt.Println(stats, avg(stats), count) //[17, 18, 15, 18, 17, 18]
 }
 
 func roll() int {
 	return rand.Intn(6) + 1
 }
 
-func getAttribute() []int{
-	rolls := make([]int, 4)
-	for i:=0; i<4; i++ {
+func getAttribute() (rolls []int) {
+	rolls = make([]int, 4)
+	for i := 0; i < 4; i++ {
 		rolls[i] = roll()
 	}
-	
+
 	//find min
 	min := rolls[0]
 	index := 0
-	for i, v := range rolls{
-		if v < min{
+	for i, v := range rolls {
+		if v < min {
 			min = v
 			index = i
 		}
 	}
-	
+
 	//remove min
 	rolls[index] = rolls[len(rolls)-1]
 	rolls = rolls[:len(rolls)-1]
-	
-	return rolls	
+
+	return
 }
 
-func getStats() []int {
-	stats := make([]int, 6)
+func getStats() (stats []int){
+	stats = make([]int, 6)
 	for i, _ := range stats {
 		stats[i] = sum(getAttribute())
 	}
-	return stats
+	return
 }
 
-func sum(arr []int) int {
-	total := 0
+func sum(arr []int) (total int) {
+	total = 0
 	for _, v := range arr {
 		total += v
 	}
-	return total
+	return
 }
 
 func avg(arr []int) float64 {
 	return float64(sum(arr)) / float64(len(arr))
 }
+
